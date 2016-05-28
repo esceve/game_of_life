5e828008f9ae0957f3553c228344fe7cb6a06028
guerin_q <guerin_q@epitech.eu>
2016-05-28 18:13:28 +0200

    Ajout de la gestion des events

---
+++ libraries/SDL_Context/srcs/windowevent.c
@@ -0,0 +1,21 @@
+/*
+** windowevent.c for SDLContext in /home/guerin_q/rendu/game_of_life/libraries/SDL_Context/srcs
+** 
+** Made by Quentin Guerin
+** Login   <guerin_q@epitech.net>
+** 
+** Started on  Sat May 28 18:07:56 2016 Quentin Guerin
+** Last update Sat May 28 18:12:52 2016 Quentin Guerin
+*/
+
+#include		"SDLContext.h"
+
+void			SDLContext_windowevent(void *context_ptr,
+					       SDL_Event *event)
+{
+  struct SDLContext	*context;
+
+  context = context_ptr;
+  (void)context;
+  (void)event;
+}
